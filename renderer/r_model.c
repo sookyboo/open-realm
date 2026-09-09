2b4914d460cb1a7509b174c03ca59dd083c25264
sookyboo <sookyboo@gmail.com>
2026-09-09 21:03:57 +0100

    add logs

--- renderer/r_model.c
+++ renderer/r_model.c
@@ -125,11 +125,3 @@ LPCSTR R_ModelDebugName(LPCMODEL model) {
     }
     return "";
 }
-
-LPCSTR R_ModelDebugName(LPCMODEL model) {
-    if (!model) return "";
-    FOR_LOOP(i, MAX_MOD_KNOWN) {
-        if (mod_known[i].model == model) return mod_known[i].name;
-    }
-    return "";
-}
