11c8c74e87cc230cbfd941a5e00edce2881ae72a
sookyboo <sookyboo@gmail.com>
2026-09-10 00:26:51 +0100

    l

--- games/warcraft-3/game/g_doodad.c
+++ games/warcraft-3/game/g_doodad.c
@@ -9,8 +9,32 @@
  */
 #include "g_local.h"
 #include <float.h>
+#include <stdlib.h>
 
 static umove_t doodad_scripted_move = { "stand", NULL, G_DoodadAnimationEnd };
+
+static BOOL G_DoodadAnimationDebugEnabled(void) {
+    LPCSTR value = gi.CvarString("wc3_animation_debug", "0");
+    return value && atoi(value) >= 2;
+}
+
+static void G_DoodadAnimationDebugCandidate(LPCEDICT ent, DWORD doodad_id,
+                                            LPCSTR anim_name, LPCBOX2 rect) {
+    LPCANIMATION anim;
+
+    if (!G_DoodadAnimationDebugEnabled() || !ent || ent->class_id != doodad_id) return;
+    anim = G_GetAnimationVariant(ent->s.model, anim_name, false);
+    fprintf(stderr,
+            "WC3_DOODAD_ANIM candidate ent=%d class=%.4s pos=(%.1f %.1f) in_rect=%d "
+            "static=%d doodad_row=%d destructable=%d model=%u request=\"%s\" resolved=\"%s\"\n",
+            ent->s.number, (LPCSTR)&ent->class_id, ent->s.origin2.x, ent->s.origin2.y,
+            rect ? Box2_containsPoint(rect, &ent->s.origin2) : 0,
+            !!(ent->svflags & SVF_STATIC_SCENERY),
+            !!(ent->data.Doodads && ent->data.Doodads->id == ent->class_id),
+            G_IsDestructable(ent), ent->s.model, anim_name ? anim_name : "",
+            anim && anim->name ? anim->name : "<none>");
+}
+
 
 BOOL G_IsDoodad(LPCEDICT ent) {
     return ent && ent->inuse && ent->class_id && (ent->svflags & SVF_STATIC_SCENERY) &&
@@ -98,13 +122,25 @@ DWORD G_SetDoodadAnimationRect(LPCBOX2 rect, DWORD doodad_id,
 
     if (!rect || !doodad_id || !anim_name || !*anim_name) return 0;
 
+    if (G_DoodadAnimationDebugEnabled()) {
+        fprintf(stderr,
+                "WC3_DOODAD_ANIM rect-call class=%.4s rect=(%.1f %.1f)-(%.1f %.1f) "
+                "request=\"%s\" random=%d edicts=%u\n",
+                (LPCSTR)&doodad_id, rect->min.x, rect->min.y, rect->max.x, rect->max.y,
+                anim_name, random_animation, globals.num_edicts);
+    }
+
     FOR_LOOP(i, globals.num_edicts) {
         LPEDICT ent = g_edicts + i;
 
+        G_DoodadAnimationDebugCandidate(ent, doodad_id, anim_name, rect);
         if (!G_IsDoodad(ent) || ent->class_id != doodad_id ||
             !Box2_containsPoint(rect, &ent->s.origin2))
             continue;
         if (G_DoodadSetAnimation(ent, anim_name, random_animation)) changed++;
     }
+    if (G_DoodadAnimationDebugEnabled())
+        fprintf(stderr, "WC3_DOODAD_ANIM rect-result class=%.4s request=\"%s\" changed=%u\n",
+                (LPCSTR)&doodad_id, anim_name, changed);
     return changed;
 }
